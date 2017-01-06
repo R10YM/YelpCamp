d05b74a30d62782ee47678564fe0f85265ebd7a7
Meng Yuan <yuanmengr10@gmail.com>
2017-01-06 02:47:37 +0000

    let user edit or delete his/her own comment

--- routes/comments.js
+++ routes/comments.js
@@ -39,11 +39,60 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+router.get("/:comment_id/edit",checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, comment){
+       if (err) {
+           res.redirect("back");
+       } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: comment});    
+       }
+    });
+});
+
+router.put("/:comment_id",checkCommentOwnership, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err, removedComment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn (req, res, next) {
     if (req.isAuthenticated()) {
         return next();    
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+            Comment.findById(req.params.comment_id, function(err, comment){
+            if (err) {
+                console.log(err);
+                res.redirect("back");
+            } else {
+                if (comment.author.id.equals(req.user._id)) {
+                    next();   
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }    
 }
 
 module.exports = router;
