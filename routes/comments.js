7e72aad9d8e8c4eb9ad21229ae694485bb29bc8c
Meng Yuan <yuanmengr10@gmail.com>
2017-01-16 19:45:50 +0000

    add slide show to landing page

--- routes/comments.js
+++ routes/comments.js
@@ -5,17 +5,6 @@ var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 
-
-router.get("/new", middleware.isLoggedIn, function(req, res) {
-    Campground.findById(req.params.id, function(err, campground){
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", {campground: campground});
-        }
-    });
-});
-
 router.post("/", middleware.isLoggedIn, function(req, res){
     //lookup campground using ID
     Campground.findById(req.params.id, function(err, campground) {
@@ -38,26 +27,6 @@ router.post("/", middleware.isLoggedIn, function(req, res){
                 }
             });
        }
-    });
-});
-
-router.get("/:comment_id/edit",middleware.checkCommentOwnership, function(req, res){
-    Comment.findById(req.params.comment_id, function(err, comment){
-       if (err) {
-           res.redirect("back");
-       } else {
-            res.render("comments/edit", {campground_id: req.params.id, comment: comment});    
-       }
-    });
-});
-
-router.put("/:comment_id",middleware.checkCommentOwnership, function(req, res){
-    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
-        if (err) {
-            res.redirect("back");
-        } else {
-            res.redirect("/campgrounds/" + req.params.id);
-        }
     });
 });
 
